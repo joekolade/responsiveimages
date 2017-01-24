7afc6c959325054c7561e2211f778f6f12f713fe
Joe Schäfer <schaefer@scw.info>
2017-01-24 09:45:29 +0100

    commit fuer heiko

---
+++ Configuration/TypoScript/setup.ts
@@ -0,0 +1,43 @@
+tt_content.textmedia {
+    settings {
+        responsive_image_rendering {
+            layoutKey = srcset
+
+            sourceCollection {
+                10 {
+                    width = 1260m
+                    srcset = 1260w
+                }
+
+                20 {
+                    width = 960m
+                    srcset = 960w
+                }
+
+                30 {
+                    width = 720m
+                    srcset = 720w
+                }
+
+                40 {
+                    width = 640m
+                    srcset = 640w
+                }
+
+                50 {
+                    width = 360m
+                    srcset = 360w
+                }
+
+                60 {
+                    width = 320m
+                    srcset = 310w
+                    sizes = 320px
+                }
+            }
+        }
+    }
+}
+
+# sourceCollection als CASE?
+# Image width vs. Chrome
